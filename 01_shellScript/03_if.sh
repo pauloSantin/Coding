eb185aa6015b39d1fab9d76378a81e11352f6e8d
pauloSantin <88294748+pauloSantin@users.noreply.github.com>
2022-04-23 21:03:10 -0300

    Add files via upload

---
+++ 01_shellScript/03_if.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+COLOR=$1
+if [ $COLOR="blue"]
+then
+    echo "The color is blue"
+fi
+
+USER_GUESS=$2
+COMPUTER=50
+
+if [ $USER_GUESS -lt $COMPUTER]
+then
+     echo "Your are to low"
+fi
